7a2340aebfd5e13521ff23c14de5bb2ae8c90de5
kl2400030499 <2400030499@kluniversity.in>
2025-11-25 14:17:30 +0530

    added a folder of cpp(basic calculator)

---
+++ Playground/talasudeepk/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    double num1, num2;
+    char op;
+
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    double result;
+
+    
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                cout << "Error: Division by zero not allowed!" << endl;
+            } else {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            }
+            break;
+
+        default:
+            cout << "Invalid operator! Please use +, -, *, or /." << endl;
+    }
+
+    return 0;
+}
